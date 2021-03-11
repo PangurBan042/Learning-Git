68e3529ba6f04a2119d6acfb77d4b97a5e005ada
Moyse Murray <moysemurray@gmail.com>
2021-03-10 19:35:46 -0500

    Initial Commit

---
+++ Learning-Git/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learning-Git
+//
+//  Created by Franny Moyse on 3/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learning-Git/Learning_GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GitApp.swift
+//  Learning-Git
+//
+//  Created by Franny Moyse on 3/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
